c5109fbccf5bd3666caa14e834d68cc486fe9453
Daniel Z <actzendaria@gmail.com>
2014-05-30 16:05:04 +0800

    Add test case.

--- skiplist/Main.java
+++ skiplist/Main.java
@@ -1,27 +1,84 @@
 package skiplist;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import algorithms.pattree.PatriciaTree;
+import util.MyConcurrentSkipListMap;
 import util.SkiplistRotating;
 
 public class Main {
 
-    static final int threadNum = 6;
+	static final int threadNum = 6;
+	static final int totalops = 500000;
+	static int ops = totalops/threadNum;
+	static final float insert_chance = 0.6f;
+	static final float search_chance = 0.2f;
+	//static final int ops = 100000;
 
 	public static void main(String[] args) throws InterruptedException {
-        SkiplistRotating[] ss = new SkiplistRotating[threadNum];
-        SkiplistTest[] ts = new SkiplistTest[threadNum];
-        long startTime = System.currentTimeMillis();
-        int ops = 100000;
-        for (int i = 0; i < threadNum; i++) {
-            ss[i] = new SkiplistRotating();
-        }
-        for (int i = 0; i < threadNum; i++) {
-            ts[i] = new SkiplistTest(i, ops, ss[i]);
-            ts[i].start();
-        }
-        for (int i = 0; i < threadNum; i++) {
-            ts[i].join();
-        }
-        long endTime = System.currentTimeMillis();
-        System.out.println("Congratulations! time = " + (endTime - startTime) + " ms");
+		//testConcurrentSl();
+		//testTrie();
+		testNoHotSpotSl();
 	}
+	
+	public static void testNoHotSpotSl() throws InterruptedException {
+		SkiplistRotating[] ss = new SkiplistRotating[threadNum];
+		SkiplistTest[] ts = new SkiplistTest[threadNum];
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < threadNum; i++) {
+			ss[i] = new SkiplistRotating();
+		}
+		for (int i = 0; i < threadNum; i++) {
+			ts[i] = new SkiplistTest(i, ops, ss[i], insert_chance, search_chance);
+			ts[i].start();
+		}
+		for (int i = 0; i < threadNum; i++) {
+			ts[i].join();
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Congratulations! time = " + (endTime - startTime)
+				+ " ms");
+	}
+
+	/*
+	public static void testConcurrentSl() throws InterruptedException {
+		//SkiplistRotating[] ss = new SkiplistRotating[threadNum];
+		//MyConcurrentSkipListMap<Integer, Object>[] ts = new MyConcurrentSkipListMap<Integer, Object>[threadNum];
+		List<MyConcurrentSkipListMap<Integer, Object>> ss= new ArrayList<MyConcurrentSkipListMap<Integer, Object>>();		
+		ConcurrentSkiplistTest[] ts = new ConcurrentSkiplistTest[threadNum];
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < threadNum; i++) {
+			ss.add(i, new MyConcurrentSkipListMap<Integer, Object>());
+		}
+		for (int i = 0; i < threadNum; i++) {
+			ts[i] = new ConcurrentSkiplistTest(i, ops, ss.get(i), insert_chance, search_chance);
+			ts[i].start();
+		}
+		for (int i = 0; i < threadNum; i++) {
+			ts[i].join();
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Congratulations! time = " + (endTime - startTime)
+				+ " ms");
+	}
+	
+	public static void testTrie() throws InterruptedException {
+		List<PatriciaTree<Integer,Object>> ss= new ArrayList<PatriciaTree<Integer,Object>>();		
+		TrieTest[] ts = new TrieTest[threadNum];
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < threadNum; i++) {
+			ss.add(i, new PatriciaTree<Integer,Object>());
+		}
+		for (int i = 0; i < threadNum; i++) {
+			ts[i] = new TrieTest(i, ops, ss.get(i));
+			ts[i].start();
+		}
+		for (int i = 0; i < threadNum; i++) {
+			ts[i].join();
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Congratulations! time = " + (endTime - startTime)
+				+ " ms");
+	}*/
 }
--- skiplist/SkiplistTest.java
+++ skiplist/SkiplistTest.java
@@ -7,49 +7,90 @@ import util.MyConcurrentSkipListMap;
 import util.SkiplistRotating;
 import java.lang.Thread;
 import java.util.Random;
+
 //import java.util.concurrent.ConcurrentSkipListMap;
 
 public class SkiplistTest extends Thread {
 
-    int x;
-    int operations;
-    SkiplistRotating sl;
+	int x;
+	int operations;
+	SkiplistRotating sl;
+	float ops_thres, ops_thres_2;
+	int ins_count, del_count, find_count;
 
-    public SkiplistTest(int x, int operations, SkiplistRotating sl) {
-        super();
-        this.x = x;
-        this.operations = operations;
-        this.sl = sl;
-    }
-	
+	public SkiplistTest(int x, int operations, SkiplistRotating sl, float f, float f2) {
+		super();
+		this.x = x;
+		this.operations = operations;
+		this.sl = sl;
+		this.ops_thres = f;
+		this.ops_thres_2 = f2;
+		this.ins_count = 0;
+		this.del_count = 0;
+		this.find_count = 0;
+	}
+
 	public void run() {
-        System.out.println("Start thread " + currentThread().getId());
-        System.out.println(" " + x);
-        Random r = new Random();
-        SkiplistRotating sl = new SkiplistRotating();
-        for (int i = operations * x; i < operations * (x + 1); i++)
-            sl.insert(i, r.nextInt(100));
-        System.out.println("Insert completed on " + currentThread().getId());
-        for (int i = operations * x; i < operations * (x + 1); i++) {
-            int ret = sl.contains(i);
-            if (ret != 1) {
-                throw new RuntimeException();
-            }
-        }
-        System.out.println("Contains completed on " + currentThread().getId());
-        for (int i = operations * x; i < operations * (x + 1); i++) {
-            int ret = sl.delete(i);
-            if (ret != 1)
-                throw new RuntimeException();
-        }
-        for (int i = operations * x; i < operations * (x + 1); i++) {
-            int ret = sl.contains(i);
-            if (ret != 0) {
-                throw new RuntimeException();
-            }
-        }
-        System.out.println("Delete compeleted on " + currentThread().getId());
+		System.out.print("Start thread " + currentThread().getId());
+		System.out.println(" (factor: " + x + ")");
+		Random r = new Random();
+		Random ops_r = new Random();
+		// SkiplistRotating sl = new SkiplistRotating();
+		if (null == sl) {
+			System.out.println("Skiplist not initialized on thread "
+					+ currentThread().getId() + " Aborting...");
+			return;
+		}
+		
+		/* Random test */
+		for (int i = operations * x; i < operations * (x + 1); i++) {
+			float ops = ops_r.nextFloat();
+			if (ops < ops_thres) {
+				sl.insert(i, r.nextInt(100));
+				++ins_count;
+			}
+			else if (ops < ops_thres_2 + ops_thres) {
+				sl.contains(i);
+				++find_count;
+			}
+			else {
+				sl.delete(i);
+				++del_count;
+			}
+		}
+		System.out.println("Thread " + currentThread().getId() + ":");
+		System.out.println("insert: " + ins_count);
+		System.out.println("search: " + find_count);
+		System.out.println("delete: " + del_count);
+		
+		/* Sanity test */
+		/*
+		for (int i = operations * x; i < operations * (x + 1); i++)
+			sl.insert(i, r.nextInt(100));
+		System.out.println("Insert completed on " + currentThread().getId());
+		for (int i = operations * x; i < operations * (x + 1); i++) {
+			int ret = sl.contains(i);
+			if (ret != 1) {
+				throw new RuntimeException();
+			}
+		}
+		System.out.println("Contains completed on " + currentThread().getId());
+		*/
+		
+		/*
+		for (int i = operations * x; i < operations * (x + 1); i++) {
+			int ret = sl.delete(i);
+			if (ret != 1)
+				throw new RuntimeException();
+		}
+		for (int i = operations * x; i < operations * (x + 1); i++) {
+			int ret = sl.contains(i);
+			if (ret != 0) {
+				throw new RuntimeException();
+			}
+		}
+		System.out.println("Delete compeleted on " + currentThread().getId());*/
 
-        System.out.println("Finish on " + currentThread().getId());
+		System.out.println("Finish on " + currentThread().getId());
 	}
 }
